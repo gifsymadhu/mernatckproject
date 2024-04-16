2a7076afbe2bec446fbdaff24b470f19be7e33d4
gifsymadhu <gifsygm@gmail.com>
2024-04-16 08:36:59 +0530

    webpack config

---
+++ client/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Adjust the entry point as needed
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    host: '0.0.0.0', // Allow all hosts
+    port: 8080, // Adjust port as needed
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    hot: true,
+    open: true
+  }
+};
